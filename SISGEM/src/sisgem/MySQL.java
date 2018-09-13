c8968d868ca2e0bf47c5a0efcece8be2b280b2c7
alumno <alumno@192.168.132.39>
2018-09-13 15:02:49 -0300

    Hola!

---
+++ SISGEM/src/sisgem/MySQL.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisgem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alumno
+ */
+public class MySQL {
+     private static Connection Conexion;
+
+    public void MySQLConnection(String user, String pass, String db_name) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+            System.out.println("Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            Conexion.close();
+            System.out.println("Se ha finalizado la conexión con el servidor");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void insertData(String table_name, String dni, String cuil, String nombreEmpleado, String apellidoEmpleado, String domicilioEmpleado, String estado,String telefonoEmpleado, String celularEmpleado, String correoEmpleado) {
+        try {
+            String Query = "INSERT INTO " + table_name + " VALUES("
+                    + "\"" + 0 + "\", "   //campo autoincremental
+                    + "\"" + (Integer.parseInt(dni)) + "\", "
+                    
+                    + "\"" + cuil + "\", "
+                    + "\"" + nombreEmpleado + "\", "
+                    + "\"" + apellidoEmpleado + "\", "
+                    + "\"" + domicilioEmpleado + "\", "
+                    + "\"" + (Integer.parseInt(estado)) + "\", "
+                    + "\"" + (Integer.parseInt(telefonoEmpleado)) + "\", "
+                    + "\"" + (Integer.parseInt(celularEmpleado)) + "\", "
+                    + "\"" + correoEmpleado + "\")";
+                   //System.out.println(Integer.parseInt(dni));
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            JOptionPane.showMessageDialog(null, "Datos almacenados de forma exitosa");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error en el almacenamiento de datos");
+        }
+    }
+    
+    public void buscar(String table_name, String Campo, String buscar){
+       try{
+           
+               
+           String Query= "SELECT * FROM " + table_name + " where " + buscar + " ='" + Campo +"'";         
+           Statement st= Conexion.createStatement();
+           ResultSet dt= st.executeQuery(Query);
+           while(dt.next()) 
+                             {
+                             System.out.println(dt.getInt (1)+ " " + dt.getString (2));
+                             }
+            
+         
+       }catch (SQLException ex){
+           System.out.println(ex.getMessage());
+           JOptionPane.showMessageDialog(null, "No se encontro el registro");
+       }
+   }
+    
+}
---
+++ SISGEM/src/sisgem/Sisgem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisgem;
+
+/**
+ *
+ * @author alumno
+ */
+public class Sisgem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        main m = new main();
+        m.setVisible(true);
+    }
+    
+}
